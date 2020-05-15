187b408bb04d46451dfbb5203e7b7802acbb7996
Burton Johnsey <git@tonjohn.com>
2020-05-15 11:49:34 -0700

    working sidebar!
    Co-authored-by: Aaron K <kearlyaa@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <nuxt />
+    <div class="h-screen flex overflow-hidden bg-gray-100">
+      <Sidebar />
+      <div class="flex flex-col w-0 flex-1 overflow-hidden">
+        <main
+          class="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6"
+          tabindex="0"
+        >
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+          </div>
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <!-- Replace with your content -->
+            <div class="py-4">
+              <nuxt />
+              <!-- <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div> -->
+            </div>
+            <!-- /End replace -->
+          </div>
+        </main>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import Sidebar from "~/components/Sidebar";
+
+export default {
+  components: {
+    Sidebar
+  }
+};
+</script>
+
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
---
+++ pages/fit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div>Today, May 5 2020</div>
+    <div class="current-user">100/250</div>
+    <div class="group-progress">
+      <div v-for="idx in 3" :key="idx" class="user">
+        <div class="progress-indicator">76%</div>
+        <div class="details">
+          <div>
+            Zach Eller
+            <span>+30</span>
+          </div>
+          <div>Last Updated 2 days ago</div>
+          <div>190</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
