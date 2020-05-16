42acd2764be2b2ed2c6bbb1e2ac8d3961e995908
Aaron <kearlyaa@gmail.com>
2020-05-16 15:07:10 -0700

    Refactored some dashboard items to individual components.  Created temp pages for profile and team.  Hooked up some link routing in the sidebar.

---
+++ pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="max-w-7xl pt-12 md:pt-0">
+      <h1 class="text-2xl font-semibold text-gray-900">Profile</h1>
+    </div>
+    <div class="flex flex-col">
+      Profile page form goes here
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
