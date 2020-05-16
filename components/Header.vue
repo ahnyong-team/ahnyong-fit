8c4f4909719a9cdbf8f858e6539e74b4f1be5788
Burton Johnsey <git@tonjohn.com>
2020-05-16 12:03:35 -0700

    unused header component

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-8 bg-gray-800">
+    <div class="max-w-7xl mx-auto">
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="flex-1 min-w-0">
+          <h2
+            class="text-2xl font-bold leading-7 text-white sm:text-3xl sm:leading-9 sm:truncate"
+          >
+            Fit Buddies
+          </h2>
+        </div>
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+          <span class="shadow-sm rounded-md">
+            <button
+              type="button"
+              class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:shadow-outline-gray focus:border-gray-700 active:bg-gray-700 transition duration-150 ease-in-out"
+            >
+              + Add Reps
+            </button>
+          </span>
+          <span class="ml-3 shadow-sm rounded-md">
+            <button
+              type="button"
+              class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-600 active:bg-indigo-600 transition duration-150 ease-in-out"
+            >
+              Shout at Buddies
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
